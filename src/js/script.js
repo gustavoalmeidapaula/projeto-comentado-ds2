4d8200fa39b82fc6f96471c2f2e29c5423481cd9
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-08-28 16:26:29 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+let char = new Knight('John')
+let monster = new LittleMonster()
+
+console.log(char.name)
+console.log(char.life)
+console.log(char.attack)
+
+console.log(monster.name)
+console.log(monster.life)
+console.log(monster.attack)
