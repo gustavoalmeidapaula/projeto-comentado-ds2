c4ee6134716372d79a7c080b09afdc991cdd2286
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-08-30 08:29:04 -0300

    Add files via upload

--- src/components/monsters/LittleMonster.js
+++ src/components/monsters/LittleMonster.js
@@ -1,7 +1,7 @@
 class LittleMonster extends Character {
     constructor(){
         super('Little Monster')
-        this.life = 100
+        this.life = 80
         this.attack = 10
         this.defense = 8
         this.maxLife = this.life
--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 let char = new Knight('John')
 let monster = new LittleMonster()
 
-console.log(char.name)
-console.log(char.life)
-console.log(char.attack)
+const stage = new Stage(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+)
 
-console.log(monster.name)
-console.log(monster.life)
-console.log(monster.attack)
+stage.start()
